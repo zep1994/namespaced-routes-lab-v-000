53825ce217f584053ea4dec31d285d8e1244562a
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-15 19:23:37 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,10 +1,13 @@
-class Admin::PreferencesController < ApplicationController
-    def index
-        @preferences = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "ASC", allow_create_artists: true, allow_create_songs: true)
-    end
-    private
-
-    def post_params
-        params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
-      end
-end
+def index
+    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
+  end
+  def update
+    @preference = Preference.find(params[:id])
+    @preference.update(preference_params)
+    redirect_to admin_preferences_path
+  end
+  private
+  def preference_params
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
+  end
+end 
